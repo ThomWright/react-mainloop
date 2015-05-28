288c1b899755a981adce308ec1126a77f9fd6360
ThomWright <github@thomwright.co.uk>
2015-05-28 13:51:47 +0100

    Initial commit for animator

---
+++ src/Animator.jsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import MainLoop from 'mainloop.js';
+
+const FPS = 60;
+const MAX_FPS = 60;
+
+/**
+ * A wrapper to animate a given component.
+ * All props are passed down to the child component.
+ * The update callback takes the elapsed time (in milliseconds) since the last update,
+ * and returns the props for the animated component.
+ *
+ * @param  {ReactComponent} AnimatedComponent
+ * @param  {function} update
+ * @return {ReactComponent} An animated version of the given component.
+ */
+export default (fps = FPS, maxFPS = MAX_FPS) => (AnimatedComponent, update) => {
+
+  const timestep = 1000 / fps;
+
+  class Animator extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.update = this.update.bind(this);
+      this.endOfFrame = this.endOfFrame.bind(this);
+
+      const loop = MainLoop
+        .setMaxAllowedFPS(maxFPS)
+        .setSimulationTimeStep(timestep)
+        .setUpdate(this.update)
+        .setDraw(this.forceUpdate)
+        .setEnd(this.endOfFrame);
+
+      this.state = {
+        loop,
+        animatedProps: {}
+      };
+    }
+
+    componentDidMount() {
+      if (this.props.run) {
+        this.state.loop.start();
+      }
+    }
+
+    componentWillReceiveProps(nextProps) {
+      if (nextProps.run) {
+        this.state.loop.start();
+      } else {
+        this.state.loop.stop();
+      }
+    }
+
+    shouldComponentUpdate() {
+      // doesn't affect initial render
+      return false; // take control of when we render using forceUpdate
+    }
+
+    componentWillUnmount() {
+      this.state.loop.stop();
+    }
+
+    update(delta) {
+      this.setState({
+        animatedProps: update(delta)
+      });
+    }
+
+    endOfFrame(_/*fps*/, panic) {
+      if (panic) {
+        this.state.loop.resetFrameDelta();
+      }
+    }
+
+    render() {
+      return (
+        <AnimatedComponent
+          {...this.props}
+          {...this.state.animatedProps}
+        />
+      );
+    }
+  }
+
+  Animator.propTypes = {
+    run: React.PropTypes.bool
+  };
+
+  Animator.defaultProps = {
+    run: true
+  };
+
+  return Animator;
+};
