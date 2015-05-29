5c05ef6bd424507e149acd65bf60ca40dbdaf106
ThomWright <github@thomwright.co.uk>
2015-05-29 11:02:25 +0100

    First basic test

--- src/__tests__/Animator-test.js
+++ src/__tests__/Animator-test.js
@@ -1,4 +1,39 @@
 
-/*global jest*/
-jest.dontMock('../Animator.jsx');
+/*global describe*/
+/*global it*/
+/*global after*/
+// global afterEach
+/*global expect*/
 
+import React from 'react/addons';
+import Animator from '../Animator.jsx';
+
+const TestUtils = React.addons.TestUtils;
+
+const animate = new Animator();
+
+const SimpleComponent = React.createClass({
+  render: function(){
+    return (
+      <div></div>
+    );
+  }
+});
+
+describe('Animator', function() {
+
+  describe('animate()', function() {
+
+    it('should successfully create a renderable component', function() {
+      let component = null;
+      after(function() {
+        React.unmountComponentAtNode(React.findDOMNode(component).parentNode);
+      });
+
+      expect(() => {
+        const AnimatedSimpleComponent = animate(SimpleComponent, () => {});
+        component = TestUtils.renderIntoDocument(<AnimatedSimpleComponent />);
+      }).to.not.throw();
+    });
+  });
+});
